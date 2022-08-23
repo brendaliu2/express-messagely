732daa8dcde338229ffad0f6acf5103bd72295d2
Brenda Liu <brenda.liu24@gmail.com>
2022-08-22 19:25:17 -0500

    step 4

--- routes/messages.js
+++ routes/messages.js
@@ -3,6 +3,10 @@
 const Router = require("express").Router;
 const router = new Router();
 
+const Message = require('../models/message');
+const { ensureCorrectUser, ensureLoggedIn } = require('../middleware/auth');
+const { UnauthorizedError } = require('../expressError');
+
 /** GET /:id - get detail of message.
  *
  * => {message: {id,
@@ -15,6 +19,15 @@ const router = new Router();
  * Makes sure that the currently-logged-in users is either the to or from user.
  *
  **/
+router.get('/:id', ensureLoggedIn, async function (req, res, next) {
+  const message = await Message.get(req.params.id);
+
+  if (res.locals.user === message.to_user.username
+    || res.locals.user === message.from_user.username) {
+    return res.json({ message });
+  }
+  throw new UnauthorizedError('Not authorized to see these messages!');
+});
 
 
 /** POST / - post message.
@@ -23,6 +36,14 @@ const router = new Router();
  *   {message: {id, from_username, to_username, body, sent_at}}
  *
  **/
+router.post('/', ensureLoggedIn, async function (req, res, next) {
+  const { to_username, body } = req.body;
+
+  const currUser = res.locals.user;
+  const message = await Message.create(currUser, to_username, body);
+
+  return res.json({ message });
+});
 
 
 /** POST/:id/read - mark message as read:
@@ -32,6 +53,16 @@ const router = new Router();
  * Makes sure that the only the intended recipient can mark as read.
  *
  **/
+router.post('/:id/read', ensureLoggedIn, async function (req, res, next) {
+  const message = await Message.get(req.params.id);
+
+  if (res.locals.user === message.to_user.username) {
+    const messageRead = Message.markRead(req.param.id)
+    return res.json({ message: messageRead });
+    
+  }
+  throw new UnauthorizedError('Not authorized to mark as read!');
+})
 
 
 module.exports = router;
